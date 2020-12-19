aff7bab914afef497e99e9fc0d4846f4a559aa79
shunafuku <74655911+shunafuku@users.noreply.github.com>
2020-12-19 21:45:08 +0900

    Update audio.py

--- module/audio.py
+++ module/audio.py
@@ -2,4 +2,4 @@ import subprocess
 
 
 def res_audio(audio_path) -> None:
-    subprocess.call("aplay -D hw:1 " + str(audio_path), shell=True)
+    subprocess.run(['aplay', '-D', 'hw:1', str(audio_path)])
